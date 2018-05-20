bd397366b4c73ca91637c3e931776a68388b97e0
Bülent Keskin <bulentkeskin@gmail.com>
2018-05-20 16:14:54 -0700

    events added

--- src/Note.js
+++ src/Note.js
@@ -4,12 +4,27 @@ import FaTrash from 'react-icons/lib/fa/trash'
 
 class Note extends Component {
 
+    constructor(props){
+        super(props)
+        this.edit = this.edit.bind(this);
+        this.remove = this.remove.bind(this);
+    }
+
+    edit(){
+        alert('editing')
+    }
+    remove(){
+        alert('removing')
+    }
+
     render(){
         return (
             <div className="note">
                 <p>Learn React</p>
-                <button><FaPencil/></button>
-                <button><FaTrash/></button>
+                <span>
+                    <button id="edit" onClick={this.edit}><FaPencil/></button>
+                    <button id="remove" onClick={this.remove}><FaTrash/></button>
+                </span>
             </div>
         )
     }
